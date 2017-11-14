15dda0e57ee2ebdf95cb04cd3d60de440658b61f
Asad <asadqb16@gmail.com>
2017-11-14 16:46:27 +0800

    Dummy UI for 2 main activities of our app. Nothing works yet.

---
+++ app/src/main/java/com/example/asad/discoverfoodusm/RestaurantProfile.java
@@ -0,0 +1,17 @@
+package com.example.asad.discoverfoodusm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Asad on 14/11/2017.
+ */
+
+public class RestaurantProfile extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.restaurantprofile);
+    }
+}
